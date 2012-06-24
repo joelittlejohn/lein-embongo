45321e626b60e2830c51d93009ee2e80ecef197c
Joe Littlejohn <joelittlejohn@gmail.com>
2012-06-25 00:40:28 +0100

    Add project definition

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject lein-embongo "0.1.0-SNAPSHOT"
+  :description "lein plugin wrapper for the flapdoodle.do embedded MongoDB API"
+  :url "https://github.com/joelittlejohn/lein-embongo"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[de.flapdoodle.embedmongo/de.flapdoodle.embedmongo "1.16"]]
+  :eval-in-leiningen true)
