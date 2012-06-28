43bf584f8ce4d2476665320c7934071ca4398f58
Joe Littlejohn <joelittlejohn@gmail.com>
2012-06-28 20:19:29 +0100

    lein-release plugin: preparing 0.1.2 release

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject lein-embongo "0.1.2-SNAPSHOT"
+(defproject lein-embongo "0.1.2"
   :description "lein plugin wrapper for the flapdoodle.do embedded MongoDB API"
   :url "https://github.com/joelittlejohn/lein-embongo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[de.flapdoodle.embedmongo/de.flapdoodle.embedmongo "1.16"]]
   :eval-in-leiningen true
-  :plugins [[lein-release "1.0.0"]]
+  :plugins [[lein-release "1.0.1-SNAPSHOT"]]
   :lein-release {:deploy-via :clojars})
