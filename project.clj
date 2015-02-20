1c0ff5299c1bf09c1fd3349d32a9873a3ad19cdc
Joe Littlejohn <joelittlejohn@gmail.com>
2015-02-20 22:58:29 +0000

    Remove lein-release in favour of core release task

--- project.clj
+++ project.clj
@@ -1,13 +1,12 @@
-(defproject lein-embongo "0.2.1"
+(defproject lein-embongo "0.2.2-SNAPSHOT"
   :description "lein plugin wrapper for the flapdoodle.do embedded MongoDB API"
   :url "https://github.com/joelittlejohn/lein-embongo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[de.flapdoodle.embed/de.flapdoodle.embed.mongo "1.31"]]
   :eval-in-leiningen true
-  :plugins [[lein-release "1.0.4"]]
-  :lein-release {:deploy-via :clojar
-                 :clojars-url "clojars@clojars.org:"}
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]]
 
   :embongo {:port 37017
             :data-dir "/tmp/xxx"
