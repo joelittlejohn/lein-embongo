6e7954a5c73ff512e98285452008787f375cf0aa
Joe Littlejohn <joelittlejohn@gmail.com>
2015-02-20 23:18:23 +0000

    Version 0.2.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-embongo "0.2.2"
+(defproject lein-embongo "0.2.3-SNAPSHOT"
   :description "lein plugin wrapper for the flapdoodle.do embedded MongoDB API"
   :url "https://github.com/joelittlejohn/lein-embongo"
   :license {:name "Eclipse Public License"
