e09d6da76bacf83e7a23aae6bd94654f14cd7cf2
Joe Littlejohn <joelittlejohn@gmail.com>
2015-02-20 22:58:29 +0000

    Remove lein-release in favour of core release task

--- project.clj
+++ project.clj
@@ -5,9 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[de.flapdoodle.embed/de.flapdoodle.embed.mongo "1.31"]]
   :eval-in-leiningen true
-  :plugins [[lein-release "1.0.4"]]
-  :lein-release {:deploy-via :clojar
-                 :clojars-url "clojars@clojars.org:"}
+  :deploy-repositories [["releases" :clojars]]
 
   :embongo {:port 37017
             :data-dir "/tmp/xxx"
