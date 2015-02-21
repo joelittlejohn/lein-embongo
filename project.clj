fb2c566de3fa2e98a41eca3c4587a6d473fc2d14
Joe Littlejohn <joelittlejohn@gmail.com>
2015-02-21 00:08:18 +0000

    Upgrade flapdoodle API to 1.46.4

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/joelittlejohn/lein-embongo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[de.flapdoodle.embed/de.flapdoodle.embed.mongo "1.31"]]
+  :dependencies [[de.flapdoodle.embed/de.flapdoodle.embed.mongo "1.46.4"]]
   :eval-in-leiningen true
   :repositories [["releases" {:url "https://clojars.org/repo"
                               :creds :gpg}]]
--- src/leiningen/embongo.clj
+++ src/leiningen/embongo.clj
@@ -2,11 +2,11 @@
   (:require [clojure.string :as string]
             [clojure.java.io :as io]
             [leiningen.core.main :as main])
-  (:import [de.flapdoodle.embed.mongo Command MongodExecutable MongodProcess MongodStarter MongodStarter]
-           [de.flapdoodle.embed.mongo.config RuntimeConfigBuilder MongodConfig AbstractMongoConfig]
-           [de.flapdoodle.embed.mongo.distribution Version]
+  (:import [de.flapdoodle.embed.mongo Command MongodStarter]
+           [de.flapdoodle.embed.mongo.config RuntimeConfigBuilder MongodConfigBuilder Net Storage]
+           [de.flapdoodle.embed.mongo.distribution Feature Version Versions]
            [de.flapdoodle.embed.process.config.io ProcessOutput]
-           [de.flapdoodle.embed.process.distribution GenericVersion]
+           [de.flapdoodle.embed.process.distribution IVersion]
            [de.flapdoodle.embed.process.io IStreamProcessor NamedOutputStreamProcessor]
            [de.flapdoodle.embed.process.runtime Network]
            [java.net InetSocketAddress Proxy Proxy$Type ProxySelector]))
@@ -41,26 +41,37 @@
       (.processOutput logger)
       (.build)))
 
+(defn- parse-version [v]
+  (try
+    (Version/valueOf v)
+    (catch IllegalArgumentException e
+      (Versions/withFeatures (reify IVersion (asInDownloadPath [_] v)) (make-array Feature 0)))))
+
 (defn- start-mongo [version port data-dir]
   (.. MongodStarter
       (getInstance runtime-config)
-      (prepare (MongodConfig. version port (Network/localhostIsIPv6) data-dir))
+      (prepare (.. (MongodConfigBuilder.)
+                   (version (parse-version version))
+                   (net (Net. port (Network/localhostIsIPv6)))
+                   (replication (Storage. data-dir nil 0)) (build)))
       (start)))
 
-(defn- config-value [project k default]
+(defn- config-value [project k & [default]]
   (get (project :embongo) k default))
 
 (defn embongo
   "Start an instance of MongoDB, run the given task, then stop MongoDB"
   [project & args]
   (let [port (config-value project :port 27017)
-        version (GenericVersion. (config-value project :version "2.4.3"))
-        data-dir (get-in project [:embongo :data-dir])
-        proxy-host (get-in project [:embongo :download-proxy-host])
+        version (config-value project :version "2.4.3")
+        data-dir (config-value project :data-dir)
+        proxy-host (config-value project :download-proxy-host)
         proxy-port (config-value project :download-proxy-port 80)]
     (when (not (nil? proxy-host)) (add-proxy-selector! proxy-host proxy-port))
+    (println "lein-embongo: starting mongo" port version (or data-dir ""))
     (let [mongod (start-mongo version port data-dir)]
       (if (seq args)
-        (try (main/apply-task (first args) project (rest args))
-             (finally (.stop mongod)))
+        (try
+          (main/apply-task (first args) project (rest args))
+          (finally (.stop mongod)))
         (while true (Thread/sleep 5000))))))
